4ea532a87a9f1b2e800aee82adef84e54e90809f
kinoko5 <ajqng0@gmail.com>
2017-04-18 04:37:24 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     Rails.application.routes.draw do
       root to: 'tasks#index'  
+      #追加
+      get 'login', to: 'sessions#new'
+      post 'login', to: 'sessions#create'
+      delete 'logout', to: 'sessions#destroy'
+
+      get 'signup', to: 'users#new'
+      resources :users, only: [:index, :show, :new, :create]
+      #ここまで追加
       resources :tasks
     end
 end
