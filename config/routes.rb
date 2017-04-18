1a60bd05aba25b358172fbda5e51413f105a892b
kinoko5 <ajqng0@gmail.com>
2017-04-18 04:55:59 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     Rails.application.routes.draw do
-      root to: 'tasks#index'  
+  get 'toppages/index'
+
+      #root to: 'tasks#index'  
       #追加
+      root to: 'toppages#index'
       get 'login', to: 'sessions#new'
       post 'login', to: 'sessions#create'
       delete 'logout', to: 'sessions#destroy'
